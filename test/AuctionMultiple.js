92cdff35963f519d3f26a2224f336b96de12c102
astralship genesis <mstefanow@gmail.com>
2018-06-28 14:05:01 +0200

    moar tests

--- test/AuctionMultiple.js
+++ test/AuctionMultiple.js
@@ -7,7 +7,8 @@ contract('AuctionMultiple', function (accounts) {
   let bidderA = accounts[1]
   let bidderB = accounts[2]
   let bidderC = accounts[3]
-  let beneficiary = accounts[4]
+  let bidderD = accounts[4]
+  let beneficiary = accounts[5]
   
   let day = 24 * 60 * 60;
   let duration = 3 * day; // similar amount to `increaseTimeIfBidBeforeEnd`
@@ -19,10 +20,12 @@ contract('AuctionMultiple', function (accounts) {
   let newBidId1 = 1;
   let newBidId2 = 2;
   let newBidId3 = 3;
+  let newBidId4 = 4;
 
   let contribution1 = 1e18;
   let contribution2 = 2e18;
   let contribution3 = 1.5e18; // in between
+  let contribution4 = 2.5e18;
 
   beforeEach(async function() {
     timestampEnd = web3.eth.getBlock('latest').timestamp  +  duration; // 1 hour from now
@@ -140,6 +143,95 @@ contract('AuctionMultiple', function (accounts) {
     assert.equal(tailBid[1].toNumber(), newBidId2);
   });
 
+  it('Should allow 3 guys bidding moar and moar (increasing bid just like last time but more action)', async function() {
+    let moar = 1.1e18;
+    await auction.sendTransaction({ value: contribution1, from: bidderA });
+    await auction.sendTransaction({ value: contribution2, from: bidderB });
+    await auction.sendTransaction({ value: contribution3, from: bidderC });
+    await auction.sendTransaction({ value: moar, from: bidderA });
 
+    var newBid1 = await auction.bids.call(newBidId1);
+    var newBid2 = await auction.bids.call(newBidId2);
+    var newBid3 = await auction.bids.call(newBidId3);
+    var headBid = await auction.bids.call(headBidId);
+    var tailBid = await auction.bids.call(tailBidId);
+
+    assert.equal(newBid1[0].toNumber(), newBidId2);
+    assert.equal(newBid1[1].toNumber(), headBidId);
+    assert.equal(newBid1[2].toNumber(), contribution1 + moar);    
+
+    assert.equal(newBid2[0].toNumber(), newBidId3);
+    assert.equal(newBid2[1].toNumber(), newBidId1);
+    assert.equal(newBid2[2].toNumber(), contribution2);
+
+    assert.equal(newBid3[0].toNumber(), tailBidId);
+    assert.equal(newBid3[1].toNumber(), newBidId2);
+    assert.equal(newBid3[2].toNumber(), contribution3);
+
+    assert.equal(headBid[0].toNumber(), newBidId1);
+    assert.equal(headBid[1].toNumber(), tailBidId);
+
+    assert.equal(tailBid[0].toNumber(), headBidId);
+    assert.equal(tailBid[1].toNumber(), newBidId3);
+
+    await auction.sendTransaction({ value: moar, from: bidderC });
+    // after sending now the standing is: C (1.5 + 1.1) A (1.0 + 1.1) B (2.0)
+
+    var newBid1 = await auction.bids.call(newBidId1);
+    var newBid2 = await auction.bids.call(newBidId2);
+    var newBid3 = await auction.bids.call(newBidId3);
+    var headBid = await auction.bids.call(headBidId);
+    var tailBid = await auction.bids.call(tailBidId);
+
+    assert.equal(newBid1[0].toNumber(), newBidId2);
+    assert.equal(newBid1[1].toNumber(), newBidId3);
+    assert.equal(newBid1[2].toNumber(), contribution1 + moar);    
+
+    assert.equal(newBid2[0].toNumber(), tailBidId);
+    assert.equal(newBid2[1].toNumber(), newBidId1);
+    assert.equal(newBid2[2].toNumber(), contribution2);
+
+    assert.equal(newBid3[0].toNumber(), newBidId1);
+    assert.equal(newBid3[1].toNumber(), headBidId);
+    assert.equal(newBid3[2].toNumber(), contribution3 + moar);
+
+    assert.equal(headBid[0].toNumber(), newBidId3);
+    assert.equal(headBid[1].toNumber(), tailBidId);
+
+    assert.equal(tailBid[0].toNumber(), headBidId);
+    assert.equal(tailBid[1].toNumber(), newBidId2);
+
+    await auction.sendTransaction({ value: contribution4, from: bidderD });
+    // final bid, this time from a new guy D (2.5)
+
+    var newBid1 = await auction.bids.call(newBidId1);
+    var newBid2 = await auction.bids.call(newBidId2);
+    var newBid3 = await auction.bids.call(newBidId3);
+    var newBid4 = await auction.bids.call(newBidId4);
+    var headBid = await auction.bids.call(headBidId);
+    var tailBid = await auction.bids.call(tailBidId);
+
+    assert.equal(newBid1[0].toNumber(), newBidId2);
+    assert.equal(newBid1[1].toNumber(), newBidId4);
+    assert.equal(newBid1[2].toNumber(), contribution1 + moar);    
+
+    assert.equal(newBid2[0].toNumber(), tailBidId);
+    assert.equal(newBid2[1].toNumber(), newBidId1);
+    assert.equal(newBid2[2].toNumber(), contribution2);
+
+    assert.equal(newBid3[0].toNumber(), newBidId4);
+    assert.equal(newBid3[1].toNumber(), headBidId);
+    assert.equal(newBid3[2].toNumber(), contribution3 + moar);    
+
+    assert.equal(newBid4[0].toNumber(), newBidId1);
+    assert.equal(newBid4[1].toNumber(), newBidId3);
+    assert.equal(newBid4[2].toNumber(), contribution4);
+
+    assert.equal(headBid[0].toNumber(), newBidId3);
+    assert.equal(headBid[1].toNumber(), tailBidId);
+
+    assert.equal(tailBid[0].toNumber(), headBidId);
+    assert.equal(tailBid[1].toNumber(), newBidId2);    
+  });
 
 });
